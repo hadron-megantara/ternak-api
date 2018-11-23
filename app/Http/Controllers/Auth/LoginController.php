3878d62ca0d7100a5f3628173cad60991f338926
Hadron Megantara <hadron@Macs-MacBook-Pro.local>
2018-11-23 11:51:04 +0700

    progress

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,37 +3,121 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+use Validator;
+use App\User;
+use App\OauthClient;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    protected function validator(array $data)
     {
-        $this->middleware('guest')->except('logout');
+        $messages = [
+            'required' => 'Field :attribute tidak boleh kosong',
+        ];
+
+        return Validator::make($data, [
+            'email' => 'required|email|max:255',
+            'password' => 'required|min:6',
+        ], $messages);
+    }
+
+    public function login(Request $request){
+        $validator = $this->validator($request->all());
+
+        if ($validator->fails())
+        {
+            foreach($validator->messages()->getmessages() as $message){
+                $errorMessage[] = $message[0];
+            }
+
+            return response()->json([
+                'success' => false,
+                'data' => null,
+                'error' => ['code' => 422, 'msg' => $errorMessage],
+                'version' => env('API_VERSION', 'v1')
+            ]);
+        }
+
+
+        dd($request->all());
+
+        if($request->has('email') && $request->has('password')){
+            $user_login = request(['email', 'password']);
+
+            $user = User::select('users.*', 's.sales_code', 's.sales_phone', 's.sales_address', 'a.area_name')->join('ms_sales as s', 's.id', '=', 'users._ms_sales')->join('ms_areas as a', 's.sales_area_id', '=', 'a.id')->where('users.email', $request->email)->first();
+
+            if(!$user){
+                return response()->json([
+                    'success' => false,
+                    'data' => null,
+                    'error' => ['code' => 404, 'msg' => "User not found"],
+                    'version' => env('API_VERSION', 'v1')
+                ]);
+            }
+
+            if(!Hash::check($request->password, $user->password)){
+                return response()->json([
+                    'success' => false,
+                    'data' => null,
+                    'error' => ['code' => null, 'msg' => "Wrong password"],
+                    'version' => env('API_VERSION', 'v1')
+                ]);
+            }
+
+            $Oauth = OauthClient::where('user_id',$user->id)
+                                ->where('personal_access_client','0')
+                                ->first();
+
+            $client = new Client();
+            $GuzzleBody = [
+                "client_id" => $Oauth->id,
+                "client_secret" => $Oauth->secret ,
+                "grant_type" =>  "password",
+                "username" => $user_login['email'],
+                "password" => $user_login['password'],
+                "scope" =>  "*",
+            ];
+
+            $api_host = env('APP_URL','');
+            $endpoint = $api_host . '/oauth/token';
+            try {
+                $clientResponse = $client->post( $endpoint, [ 'form_params' => $GuzzleBody ] );
+                $userToken = json_decode( (string) $clientResponse->getBody() );
+
+                Carbon::setLocale('Asia/Jakarta');
+                $currDate = Carbon::now()->toDateTimeString();
+
+                $user->last_login = $currDate;
+                $user->save();
+
+                $res = ['user' => $user, 'token' => $userToken];
+
+                return response()->json([
+                    'success' => true,
+                    'data' => $res,
+                    'error' => null,
+                    'version' => env('API_VERSION', 'v1')
+                ]);
+            } catch ( GuzzleHttp\Exception\BadResponseException $e ) {
+                return response()->json([
+                    'success' => false,
+                    'data' => null,
+                    'error' => ['code' => null, 'msg' => "Service error, please try again."],
+                    'version' => env('API_VERSION', 'v1')
+                ]);
+            }
+        } else{
+            return response()->json([
+                'success' => false,
+                'data' => null,
+                'error' => ['code' => null, 'msg' => "User or Password can not be empty"],
+                'version' => env('API_VERSION', 'v1')
+            ]);
+        }
+
     }
 }
